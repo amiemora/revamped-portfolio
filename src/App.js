915540bb6528d292a2cfe1aaa4aac83a3d36d00f
Amie Morales <xamiemmorales@gmail.com>
2022-09-08 00:07:57 -0500

    header and footer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg'
 import './App.css'
 import axios from 'axios'
 import {useState, useEffect, useCallback} from 'react'
-
+import Footer from './components/footer'
 import bootstrap from 'bootstrap'
 
 function App() {
@@ -10,10 +10,32 @@ function App() {
 
 
 return (
-  <>
-   
-
-  </>
+  <div className='App'>
+    <header className='header'>
+      <img src="https://i.imgur.com/dcbYgL9.jpg[/img]" alt="Avatar" className="avatar"/>
+      <button className='nav-btn'>HOME</button>
+      <button className='nav-btn'>ABOUT</button>
+      <button className='nav-btn'>PROJECTS</button>
+      <button className='nav-btn'>CONTACTS</button>
+    </header>
+    <div className='body'>
+      <div className='name'>
+        <h1 className='h1-name'>Software Developer</h1>
+        <h2 className='brand-statement'>Brand Statement Here</h2>
+      </div>
+      <div>
+        <p>
+          lorem ipsum dolor sit amet, consectetur adipiscing el
+          lorem ipsum dolor sit amet, consectetur adip
+          lorem ipsum dolor sit amet, consectetur adip
+        </p>
+      </div>
+    </div>
+    <>
+    <Footer/>
+    </>
+    
+  </div>
 
 )
 }
---
+++ src/components/footer.js
@@ -0,0 +1,21 @@
+import react from 'react'
+
+const Footer = () => {
+
+    return (
+        <>
+        <nav className="fixed-bottom">
+            <div className="container-fluid">
+                <li className="footer">
+                    <a  href="https://github.com/amiemora"><i className="fa-brands fa-github fa-2xl"></i></a>
+                </li>
+                <li className="footer">
+                    <a href="https://www.linkedin.com/in/amie-morales-101b38223/"><i class="fa-brands fa-linkedin fa-2xl"></i></a>
+                </li> 
+            </div>
+        </nav>
+        </> 
+    )
+}
+
+export default Footer
