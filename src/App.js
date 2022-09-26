a5cff080cbaef24c4491cb0a7e5b70856673e330
Amie Morales <xamiemmorales@gmail.com>
2022-09-25 22:18:04 -0500

    heroku

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 
 import "./App.css"
 import React from 'react'
-import Header from "./components/Header"
 import Footer from "./components/Footer"
 import Feed from "./components/Feed"
 import About from  "./components/About"
